58f9503f1ffd7e77c3a69b7c4237e87fb4363737
M4tte78 <baillymatteo@icloud.com>
2024-09-25 14:44:26 +0200

    ajout des regex

--- php/register.php
+++ php/register.php
@@ -8,11 +8,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Récupère les données du formulaire
     $username = $_POST['username'];
     $email = $_POST['email'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash du mot de passe
+    $password = $_POST['password'];
 
-// Prépare puis éxécute la requête d'insertion
+    // Validation du mot de passe côté serveur
+    if (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/", $password)) {
+        echo "Le mot de passe doit contenir au moins 12 caractères, incluant une majuscule, une minuscule, un chiffre et un caractère spécial.";
+        exit();
+    }
+
+    // Si la validation passe, hash du mot de passe
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // Prépare puis exécute la requête d'insertion
     $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
-    $stmt->bind_param("sss", $username, $email, $password);
+    $stmt->bind_param("sss", $username, $email, $hashed_password);
 
     if ($stmt->execute()) {
         // Redirige vers la page de connexion après une inscription réussie
@@ -26,4 +35,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $conn->close();
 }
 ?>
-
